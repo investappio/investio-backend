bbeb46dd29d13c32bedfc78e7b3b9b133494079e
Teslim Olunlade <tolunlade@outlook.com>
2022-03-22 12:56:17 -0400

    Printing market and nyse listing count

--- scripts/opening.py
+++ scripts/opening.py
@@ -1,7 +1,14 @@
 import json
 from utils import iex_request
 
-symbols = json.loads(iex_request('/ref-data/symbols', 'Tpk_5d0ba7d81e624a528b6e8800be86f6b9'))
-nyse = list(filter(lambda s: s['exchange'] == 'XNYS', symbols))
+symbols = json.loads(iex_request("/ref-data/symbols"))
+nyse = list(filter(lambda s: s["exchange"] == "XNYS", symbols))
+tickers = set(map(lambda t: t["symbol"], nyse))
 
-print(nyse)
+print(len(tickers))
+
+market = json.loads(iex_request("/stock/market/previous"))
+ohlc = list(filter(lambda o: o["symbol"] in tickers, market))
+
+# print(ohlc)
+print(len(ohlc))
