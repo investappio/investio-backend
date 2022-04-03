ad275071f99884142326fdfab8c07e6375777a0e
Teslim Olunlade <tolunlade@outlook.com>
2022-04-03 17:44:30 -0400

    Added stock price search

--- src/routes/stocks.js
+++ src/routes/stocks.js
@@ -1,5 +1,5 @@
 const Router = require('@koa/router')
-const { Stock } = require('../models')
+const { Stock, Price } = require('../models')
 
 const router = new Router()
 
@@ -19,6 +19,23 @@ router.get('/search', async (ctx) => {
 
   ctx.body.success = true
   ctx.body.stocks = res
+})
+
+router.get('/price', async (ctx) => {
+  ctx.body = {}
+
+  if (!ctx.state.user) {
+    console.log(ctx.request.headers)
+    ctx.body.success = false
+    ctx.status = 401
+    return
+  }
+
+  const { query } = ctx.request.query
+  const res = await Price.search(query)
+
+  ctx.body.success = true
+  ctx.body.prices = res
 })
 
 module.exports = router.routes()
